1eaffc2fd1cafb7579021d27e368e03c64a6439c
tincho <martin.salinas@gmail.com>
2020-03-22 18:26:22 -0300

    date utils

---
+++ src/util/dates.js
@@ -0,0 +1,58 @@
+// date utils
+
+
+const chunkify = (chunkSize, src) => Array.from(
+  { length: Math.ceil(src.length/chunkSize) },
+  (_, i) => src.slice(i*chunkSize,i*chunkSize+chunkSize)
+)
+
+const plusDays = (days, date) => {
+  const newDate = new Date(date)
+  newDate.setDate(date.getDate() + days)
+  return newDate
+}
+
+export const makeCalendarRows = (year, month) => {
+
+  const firstDay = new Date(year, month, 1);
+  const lastDay = new Date(year, Number(month) + 1, 0);
+
+  const firstWeekDay = firstDay.getDay();
+  const lastWeekDay = lastDay.getDay();
+
+  // pull back first date to prev sunday
+  const firstShowingDate = (firstWeekDay > 0)
+    ? plusDays(-firstWeekDay, firstDay)
+    : new Date(firstDay)
+
+  // push last date to following saturday
+  const lastShowingDate = (lastWeekDay < 6) 
+    ? plusDays(6 - lastWeekDay, lastDay)
+    : new Date(lastDay)
+
+  // add all days that will be shown in an array
+  const days = [
+    firstShowingDate
+  ]
+
+  let i = 0;
+  while (days.slice(-1).pop() < lastShowingDate) {
+    days.push(plusDays(++i, firstShowingDate))
+  }
+
+  // then split the array in rows of 7 days
+  return chunkify(7, days)
+}
+
+
+export const localizedWeekDays = (() => {
+  const locale = navigator.language || navigator.userLanguage || "en";
+  // create a sunday
+  const d = new Date();
+  const sunday = plusDays(-d.getDay(), d)
+  const result = Array.from(
+    { length: 7 }, 
+    (_, i) => plusDays(i, sunday).toLocaleDateString(locale, { weekday: "long" })
+  )
+  return result;
+})(); // iife
---
+++ test/dates.test.js
@@ -0,0 +1,81 @@
+import { localizedWeekDays, makeCalendarRows } from '../src/util/dates'
+
+const monts = {
+  jan: 0,
+  feb: 1,
+  mar: 2,
+  apr: 3,
+  may: 4,
+  jun: 5,
+  jul: 6,
+  aug: 7,
+  sep: 8,
+  oct: 9,
+  nov: 10,
+  dec: 11
+}
+
+describe('date utilities', () => {
+  it('exports an array of week days', () => {
+    expect(localizedWeekDays.length).toBe(7)
+    expect(localizedWeekDays.every(item => typeof item === 'string')).toBe(true)
+  })
+
+  describe('calendar week rows', () => {
+
+    it('february 2020 starts on a saturday but ends on saturday', () => {
+      const weeks = makeCalendarRows(2020, months.feb)
+
+      // first showing day: january 26th
+      const firstDay = weeks[0][0]
+      expect(firstDay.getDate()).toBe(26)
+      expect(firstDay.getMonth()).toBe(months.jan)
+
+      // last showing day: february 29th
+      const lastDay = weeks.slice(-1).pop().slice(-1).pop()
+      expect(lastDay.getDate()).toBe(29)
+      expect(lastDay.getMonth()).toBe(months.feb)
+
+      // rows: 5
+      expect(weeks.length).toBe(5)
+    })
+
+
+    it('march 2020 starts on a sunday but ends on tuesday', () => {
+      const weeks = makeCalendarRows(2020, months.mar)
+
+      // first showing day: march 1st
+      const firstDay = weeks[0][0]
+      expect(firstDay.getDate()).toBe(1)
+      expect(firstDay.getMonth()).toBe(months.mar)
+
+      // last showing day: apr 4th
+      const lastDay = weeks.slice(-1).pop().slice(-1).pop()
+      expect(lastDay.getDate()).toBe(4)
+      expect(lastDay.getMonth()).toBe(months.apr)
+
+      // rows: 5
+      expect(weeks.length).toBe(5)
+    })
+
+
+
+    it('may 2020 starts on a saturday but ends on saturday', () => {
+      const weeks = makeCalendarRows(2020, months.may)
+
+      // first showing day: april 26th
+      const firstDay = weeks[0][0]
+      expect(firstDay.getDate()).toBe(26)
+      expect(firstDay.getMonth()).toBe(months.apr)
+
+      // last showing day: june 6th
+      const lastDay = weeks.slice(-1).pop().slice(-1).pop()
+      expect(lastDay.getDate()).toBe(6)
+      expect(lastDay.getMonth()).toBe(months.jun)
+
+      // rows: 6
+      expect(weeks.length).toBe(6)
+    })
+
+  })
+})
